4c12b67015f6f2efc19093e3b57bb70227ff5070
arakene <arakene_dev@naver.com>
2025-04-18 22:41:55 +0900

    docs: 목표 변경

--- android/presentation/src/main/java/com/arakene/presentation/ui/FeedDetail.kt
+++ android/presentation/src/main/java/com/arakene/presentation/ui/FeedDetail.kt
@@ -1,7 +1,12 @@
 package com.arakene.presentation.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -14,6 +19,13 @@ import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import com.arakene.domain.responses.VideoDto
 
+/**
+ *   * 영상 전체화면으로 전환
+ *   * orientation 에 따른 화면 설정
+ *   * 재생, 일시 정지, 멈춤
+ *   * 뒤로 가기, 앞으로 가기
+ *   * 다음 영상으로, 이전 영상으로
+ */
 @Composable
 fun FeedDetail(
     videoDto: VideoDto,
@@ -73,7 +85,35 @@ fun FeedDetail(
                 }
             }
         )
+
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+
+            ControlMenu(
+                play = {
+                    exoPlayer.play()
+                },
+                stop = {
+                    // TODO Stop 에 다시 Play를 하면 재생이 안된다 그 이유가 뭘까 release 되는걸까?
+                    exoPlayer.stop()
+                }
+            )
+        }
     }
 
+
+}
+
+
+@Composable
+fun ControlMenu(
+    play: () -> Unit,
+    stop: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+        Text("Play", modifier = Modifier.clickable { play() })
+        Text("Stop", modifier = Modifier.clickable { stop() })
+    }
 
 }
